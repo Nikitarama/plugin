d1f84eec269ac0f4b9f4ba238ba8242f4bda02af
Nikitarama <Nikita@crazyscientist996@gmail.com>
2023-05-19 09:06:58 +0200

    first

---
+++ plugin.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+ * Plugin Name:       Custom_Plugin
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Nikita Ramawoothar
+ 
+ */
+
+ ?>
+
+ add_filter( 'show_admin_bar', '__return_false' );
+
